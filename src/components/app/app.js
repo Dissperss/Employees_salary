aeab78ef4c7582edd9855bf7140ece42b91a2a67
Dissperss <subbotin.vladimir2018@ya.ru>
2025-12-09 12:21:26 +0300

    Props added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,14 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import "./app.css";
 
 function App() {
+    const data = [
+        { name: "Antony", salary: 800, increase: true, id: 1 },
+        { name: "Nikita", salary: 300, increase: false, id: 2 },
+        { name: "Neo", salary: 5000, increase: false, id: 3 },
+        { name: "Bea", salary: 50070, increase: true, id: 4 },
+        { name: "Leo", salary: 356, increase: false, id: 5 },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -16,7 +24,7 @@ function App() {
                 <Appfilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
